0d8f91d09ca53f8c190352220614c7a4032c5851
Akhil Sofat <akhil.sofat@warwick.ac.uk>
2023-12-03 13:19:32 +0000

    Run Lasso Regression

--- RScript.R
+++ RScript.R
@@ -1,35 +1,51 @@
 install.packages("tidyverse") 
-library(tidyverse)
-
-
-load(file="yelp_review_small.Rda")
-load("yelp_user_small.Rda")
+install.packages("caret")
+install.packages("glmnet")
 
 summary(review_data_small)
 summary(user_data_small)
 
-install.packages("caret")
+library(tidyverse)
 library(caret)
+library(glmnet)
+
+
+load(file="yelp_review_small.Rda")
+load(file="yelp_user_small.Rda")
+
+
+Combined_Data <- merge(review_data_small, user_data_small, by = "user_id")
+
+
+
+predictors <- Combined_Data[, c('useful.x', 'funny.x', 'cool.x','review_count','useful.y','funny.y','cool.y','fans','average_stars')]  
+
+target <- Combined_Data$stars
+
+
+
+predictors_matrix <- model.matrix(~ . - 1, data = predictors)
+
 
 set.seed(1)
+test_indices <- sample(1:nrow(Combined_Data), 10000)
+train_indices <- setdiff(1:nrow(Combined_Data), test_indices)
 
-n <- nrow(user_data_small)
-testSize <- 10000
-
-if (n <= testSize) {
-  stop("The dataset is too small to extract 10,000 observations for the test set.")
-}
-
-testIndex <- sample(1:n, testSize, replace = FALSE)
-
-testData <- user_data_small[testIndex, ]
-trainData <- user_data_small[-testIndex, ]
-
-install.packages("glmnet")
-
-library(glmnet)
-library(caret)
-
-Combine_data <-merge(review_data_small,user_data_small, by = "user_id")
+trainData <- predictors_matrix[train_indices, ]
+trainTarget <- target[train_indices]
+testData <- predictors_matrix[test_indices, ]
+testTarget <- target[test_indices]
 
 
+set.seed(1) # for reproducibility
+cv.lasso <- cv.glmnet(trainData, trainTarget, alpha = 1, family = "gaussian")
+bestLambda <- cv.lasso$lambda.min
+
+
+final_model <- glmnet(trainData, trainTarget, alpha = 1, lambda = bestLambda, family = "gaussian")
+
+
+predictions <- predict(final_model, s = bestLambda, newx = testData)
+
+mse <- mean((predictions - testTarget) ^ 2)
+print(paste("Mean Squared Error:", mse))
