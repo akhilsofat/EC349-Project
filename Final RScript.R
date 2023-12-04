a8ee5cab9891fbb6a537e2154c6412622e9cb77b
Akhil Sofat <akhil.sofat@warwick.ac.uk>
2023-12-04 19:04:33 +0000

    Scaling the data to see if it gets more accurate predictions

--- Final RScript.R
+++ Final RScript.R
@@ -51,12 +51,12 @@ TestTarget <- target[Test_Indices]
 #Determine the value of lambda
 
 set.seed(1)
-Lasso_Lambda <- cv.glmnet(Predictors_Matrix, target, alpha = 1, thresh = 1e-12)
+Lasso_Lambda <- cv.glmnet(Predictors_Matrix, target, alpha = 0, thresh = 1e-12)
 Best_Lambda <- Lasso_Lambda$lambda.min
 
 #Use the value of lambda in the LASSO model on the training data
 
-Lasso_Model <- glmnet(TrainData, TrainTarget, alpha = 1, lambda = Best_Lambda, thresh =1e-12)
+Lasso_Model <- glmnet(TrainData, TrainTarget, alpha = 0, lambda = Best_Lambda, thresh =1e-12)
 
 #Predict the model for the test data
 
@@ -76,5 +76,58 @@ Accuracy_Score <- sum(Accurate) / length(TestTarget)
 print(paste("Mean Squared Error (MSE):", Lasso_MSE))
 print(paste("R-squared:", R_Squared))
 print(paste("Accuracy Score:", Accuracy_Score))
+
+#Understanding which predictors are most relevant by analysing the regression coefficients
+
+Coefficients_Matrix <- as.matrix(coef(Lasso_Model, s = 1))
+
+# Convert to a data frame
+Coefficients_df <- as.data.frame(Coefficients_Matrix)
+
+Coefficients_df <- Coefficients_df[-1, , drop = FALSE]  # Remove intercept
+names(Coefficients_df) <- c("Coefficient")
+
+# Add row names as a new column (feature names)
+Coefficients_df$Feature <- rownames(Coefficients_df)
+rownames(Coefficients_df) <- NULL
+
+# View the coefficients data frame
+print(Coefficients_df)
+
+#Scale the predictors to standardise them
+
+Scaled_Predictors <- as.matrix(scale(Predictors))  # Standardize features
+
+ScaledTrainData <- Scaled_Predictors[Train_Indices, ]
+ScaledTestData <- Scaled_Predictors[Test_Indices, ]
+
+ScaledTrainTarget <- scale(TrainTarget)
+ScaledTestTarget <- scale(TestTarget)
+
+set.seed(1)
+Scaled_Lasso_Lambda <- cv.glmnet(Scaled_Predictors, target, alpha = 0, thresh = 1e-12)
+Scaled_Best_Lambda <- Lasso_Lambda$lambda.min
+
+Scaled_Lasso_Model <- glmnet(ScaledTrainData, ScaledTrainTarget, alpha = 0, lambda = Best_Lambda, thresh =1e-12)
+
+#Predict the model for the test data
+
+Scaled_Lasso_Predictions <- predict(Lasso_Model, s = Best_Lambda, newx = ScaledTestData)
+
+Scaled_Lasso_MSE <- mean((Scaled_Lasso_Predictions - ScaledTestTarget) ^ 2)
+
+Scaled_Lasso_Total_SS <- sum((ScaledTestTarget - mean(ScaledTestTarget))^2)
+Scaled_Lasso_Residual_SS <- sum((ScaledTestTarget - Scaled_Lasso_Predictions) ^ 2)
+Scaled_R_Squared <- 1 - (Scaled_Lasso_Residual_SS / Scaled_Lasso_Total_SS)
+
+Scaled_Accurate <- abs(Scaled_Lasso_Predictions - ScaledTestTarget) <= 0.25
+Scaled_Accuracy_Score <- sum(Scaled_Accurate) / length(ScaledTestTarget)
+
+print(paste("Mean Squared Error (MSE):", Scaled_Lasso_MSE))
+print(paste("R-squared:", Scaled_R_Squared))
+print(paste("Accuracy Score:", Scaled_Accuracy_Score))
+
+
+
 
 
