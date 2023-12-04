7d1149b496c2792c5cba37c87b2aa638c92c8d42
Akhil Sofat <akhil.sofat@warwick.ac.uk>
2023-12-04 17:49:27 +0000

    Set up the factors for regression

--- Final RScript.R
+++ Final RScript.R
@@ -8,6 +8,8 @@ install.packages("tidyverse")
 install.packages("caret")
 install.packages("glmnet")
 install.packages("ggplot2")
+install.packages("randomForest")
+
 
 #Call on relevant libraries
 
@@ -15,6 +17,7 @@ library(tidyverse)
 library(caret)
 library(glmnet)
 library(ggplot2)
+library(randomForest)
 
 #Load Small Data
 
@@ -24,6 +27,15 @@ load(file="yelp_user_small.Rda")
 #Create a combined dataset, merging by user id
 
 Combined_Data <- merge(review_data_small, user_data_small, by = "user_id")
+
+#Create a vector of features that will be used for the regression; Convert to matrix for regression
+
+predictors <- Combined_Data[, c('review_count', 'useful.y','funny.y','cool.y','fans','average_stars','compliment_hot','compliment_more','compliment_more','compliment_profile','compliment_cute','compliment_list','compliment_note','compliment_plain','compliment_cool','compliment_funny','compliment_writer','compliment_photos')]  
+predictors_matrix <- model.matrix(~ . - 1, data = scaled_predictors_df)
+
+#Define the target the regression is trying to predict
+
+target <- Combined_Data$stars
 
 
 
