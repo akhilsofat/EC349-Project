081666e61ec75ca5d54ddf32d076829771fb39a3
Akhil Sofat <akhil.sofat@warwick.ac.uk>
2023-12-04 16:29:05 +0000

    Install Packages and call on them

---
+++ Final RScript.R
@@ -0,0 +1,18 @@
+#Install Relevant Packages
+
+install.packages("tidyverse") 
+install.packages("caret")
+install.packages("glmnet")
+install.packages("ggplot2")
+
+#Call on relevant libraries
+
+library(tidyverse)
+library(caret)
+library(glmnet)
+library(ggplot2)
+
+
+
+
+
