ebe7e5cae455ae7ff6e844d102d25d9cb70449a3
Akhil Sofat <akhil.sofat@warwick.ac.uk>
2023-12-05 00:00:02 +0000

    Set working directory, install and load relevant libraries

---
+++ AkhilSofatRCode.R
@@ -0,0 +1,17 @@
+#Set Working Directory
+
+setwd("/Users/akhil/Documents/Economics/EC349/EC349 Project")
+
+#Install Relevant Packages
+
+install.packages("tidyverse") 
+install.packages("caret")
+install.packages("ggplot2")
+install.packages("randomForest")
+
+#Load relevant libraries
+
+library(tidyverse)
+library(caret)
+library(ggplot2)
+library(randomForest)
